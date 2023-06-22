e21f5ea52f44a620011e20bd18baf9849dfe2f5c
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-06-22 23:49:40 +0530

    api not working

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+
+const url = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'f66624530cmshd7e361d254eb304p19c792jsncf3ac59211eb',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
 	}
 };
 
+
 async function asyncCall(){
-	try {
-		const response = await fetch(url, options);
-		const result = await response.text();
-		console.log(result);
-	} catch (error) {
-		console.error(error);
-	}
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
 }
 
-console.log(asyncCall(a))
+}
+
+
