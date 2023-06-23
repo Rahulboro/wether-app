6e6aa66db948f3ee16d4dbd598434cd53232e20b
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-06-23 22:54:34 +0530

    js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 // };
 
 
-// async function asyncCall(){
+// async function async(){
 
 // try {
 // 	const response = await fetch(url, options);
@@ -18,22 +18,42 @@
 // } catch (error) {
 // 	console.error(error);
 // }
-
 // }
 
 
 
 
 
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': 'b45e96323fmshfSe4bc50d3b8a%0pled1djsnfb21lcdedeb8',
+//     'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+//   }
+// };
+
+// fetch('https://weather-by-api-ninjas.p.rapidapi.com/vl/weather?city=Seattle',)
+//   .then(response => response.json())
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
+
+
+
+
+
+const url = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5';
 const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'b45e96323fmshfSe4bc50d3b8a%0pled1djsnfb21lcdedeb8',
-    'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f66624530cmshd7e361d254eb304p19c792jsncf3ac59211eb',
+		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+	}
 };
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/vl/weather?city=Seattle', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+async function data(params) {
+	
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+	console.error(error);
+}
