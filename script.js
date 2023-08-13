5f5f89704e2a0c106e1214d28212f77b00b009ab
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-08-13 20:10:26 +0530

    new api

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=lakhimpur%20assam%20india%20';
+const url = 'https://forecast9.p.rapidapi.com/';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'f66624530cmshd7e361d254eb304p19c792jsncf3ac59211eb',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+		'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
 	}
 };
 
-async function async_get_weather(){
-	try {
-		const response = await fetch(url, options)
-		const result = await response.text();
-		console.log(result[0]);
-		let noida = document.getElementById("noida_id")
-		noida.innerHTML = result()
-	} catch (error) {
-		console.error(error);
-	}
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
 }
