2c59d0476654db5c9762c408e2814d802441f6e8
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-08-14 22:49:00 +0530

    updated axios

---
+++ Untitled-1.js
@@ -0,0 +1,41 @@
+{
+   location 
+     name = result.location.name
+     region = result.location.region
+     country = result.location.country
+     lat = result.location.lat
+     lon = result.location.lon
+     tz_id = result.location.tz_id
+     localtime_epoch = result.location.localtime_epoch
+     localtime = result.location.localtime
+  },= result.
+   current 
+     last_updated_epoch = result.
+     last_updated = result.
+     temp_c = result.
+     temp_f = result.
+     is_day = result.
+     condition = result.
+       text = result.
+       icon = result.
+       code = result.
+    },= result.
+     wind_mph 
+     wind_kph = result.
+     wind_degree = result.
+     wind_dir = result.
+     pressure_mb = result.
+     pressure_in = result.
+     precip_mm = result.
+     precip_in = result.
+     humidity = result.
+     cloud = result.
+     feelslike_c = result.
+     feelslike_f = result.
+     vis_km = result.
+     vis_miles = result.
+     uv = result.
+     gust_mph = result.
+     gust_kph = result.
+  }= result.
+}
--- script.js
+++ script.js
@@ -1,16 +1,56 @@
-const url = 'https://forecast9.p.rapidapi.com/';
+const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=lakhimpur%20assam%20india%20';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'f66624530cmshd7e361d254eb304p19c792jsncf3ac59211eb',
-		'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
+const  async_get_weather = async ()=>{
+	try {
+		const response = await fetch(url, options)
+		const result = await response.json.current(
+			);
+			let celcis = document.getElementById("name_city").innerHTML = result([2,3])
+		
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+
+	} catch (error) {
+		console.error(error);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import axios from "axios";
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+//   params: {city: 'Seattle'},
+//   headers: {
+//     'X-RapidAPI-Key': 'f66624530cmshd7e361d254eb304p19c792jsncf3ac59211eb',
+//     'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+//   }
+// };
+// const data_coming = async()=>{
+// 	try {
+// 		const response = await axios.request(options);
+// 		console.log(response.data);
+// 	} catch (error) {
+// 		console.error(error);
+// 	}
+// }
